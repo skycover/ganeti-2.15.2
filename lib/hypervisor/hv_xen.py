db69c3917c1ae00f31943c52c147c7938ce64d9c
Vladimir Ipatov <vs@ipatov.net>
2018-02-27 21:52:36 +0300

    fix for ganeti user_shutdown design for xen. if user_shutdown disabled, set domain poweroff behaviour to destroy, if enabled, set to preserve

--- lib/hypervisor/hv_xen.py
+++ lib/hypervisor/hv_xen.py
@@ -1392,7 +1392,10 @@ class XenHypervisor(hv_base.BaseHypervisor):
 
 
 def WriteXenConfigEvents(config, hvp):
-  config.write("on_poweroff = 'preserve'\n")
+  if hvp[constants.HV_XEN_USER_SHUTDOWN] == True:
+    config.write("on_poweroff = 'preserve'\n")
+  else:
+    config.write("on_poweroff = 'destroy'\n")
   if hvp[constants.HV_REBOOT_BEHAVIOR] == constants.INSTANCE_REBOOT_ALLOWED:
     config.write("on_reboot = 'restart'\n")
   else:
@@ -1426,6 +1429,7 @@ class XenPvmHypervisor(XenHypervisor):
       hv_base.ParamInSet(True, constants.KNOWN_XEN_COMMANDS),
     constants.HV_XEN_CPUID: hv_base.NO_CHECK,
     constants.HV_SOUNDHW: hv_base.NO_CHECK,
+    constants.HV_XEN_USER_SHUTDOWN: hv_base.NO_CHECK,
     }
 
   def _GetConfig(self, instance, startup_memory, block_devices):
@@ -1547,6 +1551,7 @@ class XenHvmHypervisor(XenHypervisor):
       hv_base.ParamInSet(True, constants.KNOWN_XEN_COMMANDS),
     constants.HV_XEN_CPUID: hv_base.NO_CHECK,
     constants.HV_SOUNDHW: hv_base.NO_CHECK,
+    constants.HV_XEN_USER_SHUTDOWN: hv_base.NO_CHECK,
     }
 
   def _GetConfig(self, instance, startup_memory, block_devices):
